27f33978045f5232cf5543d246ea167d99bc54dc
Filipe Botasso <56030540+fbotasso@users.noreply.github.com>
2025-06-18 10:42:23 -0300

    correcao diretorios src e public

--- src/components/Icon/Icon.js
+++
@@ -1,8 +0,0 @@
-import React, { createElement, useState } from 'react';
-
-export default function IconsDescription({ }) {
-    (    <div>
-            <i class="material-icons">description</i>Manifesto
-        </div>
-    );
-}
